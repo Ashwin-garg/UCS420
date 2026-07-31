37137dbd45ae3ff1ec230d796c71b7e3e88db4fd
Ashwin-garg <ashwingarg202106@gmail.com>
2026-07-31 14:50:22 +0530

    first lab

---
+++ first.py
@@ -0,0 +1,60 @@
+# print("Ashwin Garg")
+
+# a=10
+# b=220
+# c=a+b
+# print (c)
+
+# string= "Hello, World!"
+# string2= "Python"
+# print(string+' '+string2)
+
+# for i in range(2,10,2):
+#     print(i)
+
+# print(str(a) + ' ' + string)
+
+# print(a+b+c)
+# string3= "string3"
+
+# print (string+' ' + string2 + ' ' + string3)
+
+
+
+# n1=input("Enter first number: ")
+# n2=input("Enter second number: ")
+# n3=input("Enter third number: ")
+# print("The numbers are: ", n1, n2, n3)
+# print("The sum of the numbers is: ", int(n1)+int(n2)+int(n3))
+
+
+
+for i in range(1, 11):
+    print(i)
+
+j=0
+while (int(j)<=10):
+    print(j)
+    j+=1
+print("\n")
+for i in range(5,51,5):
+    print(i)
+
+print("\n")
+for i in range(7,71,7):
+    print(i)
+
+print("\n")
+for i in range(9,91,9):
+    print(i)
+
+
+input1=input("Enter number: ")
+print("\n")
+for i in range(1,11):
+    print(int(input1)*i)
+
+sum=0
+for i in range(1,int(input1)):
+    sum=sum+i
+print("sum=",sum)
